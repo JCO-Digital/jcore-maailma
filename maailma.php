67fef7744398f8d286441c4f69051676893dcb68
Niklas Schönberg <niklas@uplink.fi>
2025-12-09 19:10:09 +0200

    1.0.0

--- maailma.php
+++ maailma.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       JCORE Maailma
  * Description:       A global content post type and block.
- * Version: 0.1.0
+ * Version: 1.0.0
  * Requires at least: 6.7
  * Requires PHP:      8.2
  * Author:            J&Co Digital
