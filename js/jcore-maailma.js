a9d1b308a95528cd39560d16a859f69f5e609d9a
Niklas Schönberg <niklas@uplink.fi>
2026-02-23 16:39:07 +0200

    fix(maailma): add toast check and handle clipboard write errors

--- js/jcore-maailma.js
+++ js/jcore-maailma.js
@@ -1,18 +1,26 @@
 (function () {
 	const toast = document.getElementById("jcore-copy-toast");
+	if (!toast) return;
+
 	let timeout;
 
 	document.querySelectorAll(".jcore-copy-slug").forEach((el) => {
 		el.addEventListener("click", function (event) {
 			event.preventDefault();
-			const text = this.innerText;
-			navigator.clipboard.writeText(text).then(() => {
-				toast.classList.add("show");
-				clearTimeout(timeout);
-				timeout = setTimeout(() => {
-					toast.classList.remove("show");
-				}, 2000);
-			});
+			const text = this.innerContent;
+			navigator.clipboard
+				.writeText(text)
+				.then(() => {
+					toast.classList.add("show");
+					clearTimeout(timeout);
+					timeout = setTimeout(() => {
+						toast.classList.remove("show");
+					}, 2000);
+				})
+				.catch((error) => {
+					console.error("Failed to copy text to clipboard:", error);
+					alert("Copy to clipboard failed. Please copy manually.");
+				});
 		});
 	});
 })();
