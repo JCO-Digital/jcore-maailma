cc86403d4ba28f5c92370004edecde1f8a3b3491
Niklas Schönberg <niklas@uplink.fi>
2026-02-23 15:59:11 +0200

    chore(versionSync): update version constant and refactor script

--- versionSync.js
+++ versionSync.js
@@ -1,14 +1,24 @@
 const { readFileSync, writeFileSync } = require("fs");
 
 exports.preCommit = (props) => {
-  try {
-    const pluginFileName = "jcore-maailma.php";
-    const baseFile = readFileSync(pluginFileName);
-    const baseString = baseFile
-      .toString()
-      .replace(/^(.*)Version:.*$/m, `$1Version: ${props.version}`);
-    writeFileSync(pluginFileName, baseString);
-  } catch (error) {
-    console.error(error);
-  }
+	fileReplace(
+		"jcore-maailma.php",
+		/(.*)Version:( *)[\.0-9]*$/m,
+		`$1Version:$2${props.version}`,
+	);
+	fileReplace(
+		"jcore-maailma.php",
+		/define\( 'JCORE_MAAILMA_VERSION', '[0-9.]+' \);/m,
+		`define\( 'JCORE_MAAILMA_VERSION', '${props.version}' \);`,
+	);
+};
+
+const fileReplace = (filename, search, replace) => {
+	try {
+		const file = readFileSync(filename);
+		const updatedFile = file.toString().replace(search, replace);
+		writeFileSync(filename, updatedFile);
+	} catch (error) {
+		console.error(error);
+	}
 };
