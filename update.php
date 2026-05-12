97aaff802ca04e184c7994ebab09357e669fd580
Niklas Schönberg <niklas@uplink.fi>
2026-05-12 12:30:08 +0300

    fix(update): improve plugin update logic and caching
    - Add transient validation and object caching for remote metadata
    - Correct the download URL generation
    - Update zip exclusion list to track correct configuration file

--- update.php
+++ update.php
@@ -7,86 +7,101 @@
 
 namespace Jcore\Maailma;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined("ABSPATH")) {
 	exit(); // Exit if accessed directly.
 }
 
-define(
-	'JCORE_MAAILMA_RELEASE',
-	'https://github.com/JCO-Digital/jcore-maailma/releases/latest/download',
-);
+if (!defined("JCORE_MAAILMA_RELEASE")) {
+	define(
+		"JCORE_MAAILMA_RELEASE",
+		"https://github.com/JCO-Digital/jcore-maailma/releases/latest/download",
+	);
+}
 
-add_filter( 'pre_set_site_transient_update_plugins', "\Jcore\Maailma\update" );
+add_filter("pre_set_site_transient_update_plugins", __NAMESPACE__ . "\\update");
 
 /**
  * Check for updates and add them to the transient.
  *
- * @param object $transient The transient object.
+ * @param mixed $transient The transient object.
  *
- * @return object
+ * @return mixed
  */
-function update( $transient ) {
-	if ( empty( $transient->checked ) ) {
+function update($transient)
+{
+	if (!is_object($transient) || empty($transient->checked)) {
 		return $transient;
 	}
 
-	$update      = create_plugin_object();
 	$plugin_data = get_data();
+	$update = create_plugin_object($plugin_data);
 
-	if (
-		isset( $update->new_version ) &&
-		version_compare( $plugin_data['version'], $update->new_version, '<' )
-	) {
-		$transient->response[ $plugin_data['plugin'] ] = $update;
+	if (empty($update->new_version)) {
+		return $transient;
+	}
+
+	if (version_compare($plugin_data["version"], $update->new_version, "<")) {
+		$transient->response[$plugin_data["plugin"]] = $update;
 	} else {
-		$transient->no_update[ $plugin_data['plugin'] ] = $update;
+		$transient->no_update[$plugin_data["plugin"]] = $update;
 	}
 
 	return $transient;
 }
 
-function create_plugin_object(): \stdClass {
-	$plugin_data = get_data();
-
+/**
+ * Build a plugin update payload object for WordPress.
+ *
+ * @param array $plugin_data Local plugin metadata.
+ *
+ * @return \stdClass
+ */
+function create_plugin_object(array $plugin_data): \stdClass
+{
 	$remote = fetch();
 
-	if ( ! $remote ) {
-		return (object) array();
+	if (!$remote) {
+		return (object) [];
 	}
 
-	$item = (object) array(
-		'id'            => $plugin_data['id'],
-		'slug'          => $plugin_data['slug'],
-		'plugin'        => $plugin_data['plugin'],
-		'new_version'   => $remote->version,
-		'url'           => $remote->download_url ?? '',
-		'package'       => '',
-		'icons'         => array(),
-		'banners'       => array(),
-		'banners_rtl'   => array(),
-		'tested'        => '',
-		'requires_php'  => '',
-		'compatibility' => new \stdClass(),
-	);
-	return $item;
+	return (object) [
+		"id" => $plugin_data["id"],
+		"slug" => $plugin_data["slug"],
+		"plugin" => $plugin_data["plugin"],
+		"new_version" => $remote->version,
+		"url" => $remote->download_url ?? "",
+		"package" => $remote->download_url ?? "",
+		"icons" => [],
+		"banners" => [],
+		"banners_rtl" => [],
+		"tested" => "",
+		"requires_php" => "",
+		"compatibility" => new \stdClass(),
+	];
 }
 
-function get_data(): array {
-	static $data = array();
+/**
+ * Get plugin metadata from the plugin header.
+ *
+ * @return array
+ */
+function get_data(): array
+{
+	static $data = [];
 
-	if ( ! empty( $plugin_data ) ) {
+	if (!empty($data)) {
 		return $data;
 	}
 
-	$map = array(
-		'name'    => 'Plugin Name',
-		'version' => 'Version',
-	);
+	$map = [
+		"name" => "Plugin Name",
+		"version" => "Version",
+	];
 
-	$data           = get_file_data( JCORE_MAAILMA_PLUGIN_FILE, $map );
-	$data['id']     = plugin_basename( JCORE_MAAILMA_PLUGIN_FILE );
-	$data['slug']   = basename( JCORE_MAAILMA_PLUGIN_FILE, '.php' );
-	$data['plugin'] = plugin_basename( JCORE_MAAILMA_PLUGIN_FILE );
+	$data = get_file_data(JCORE_MAAILMA_PLUGIN_FILE, $map);
+	$data["id"] = plugin_basename(JCORE_MAAILMA_PLUGIN_FILE);
+	$data["slug"] = basename(JCORE_MAAILMA_PLUGIN_FILE, ".php");
+	$data["plugin"] = plugin_basename(JCORE_MAAILMA_PLUGIN_FILE);
 
 	return $data;
 }
@@ -96,35 +111,45 @@ function get_data(): array {
  *
  * @return \stdClass|bool
  */
-function fetch() {
-	// Fetch package.json from the latest release asset
-	$response = wp_remote_get(
-		JCORE_MAAILMA_RELEASE . '/package.json',
-		array(
-			'timeout' => 10,
-			'headers' => array(
-				'Accept'     => 'application/json',
-				'User-Agent' =>
-					'WordPress/' . get_bloginfo( 'version' ) . '; ' . get_bloginfo( 'url' ),
-			),
-		)
-	);
+function fetch()
+{
+	static $cached_remote = null;
+
+	if (null !== $cached_remote) {
+		return $cached_remote;
+	}
+
+	$response = wp_remote_get(JCORE_MAAILMA_RELEASE . "/package.json", [
+		"timeout" => 10,
+		"headers" => [
+			"Accept" => "application/json",
+			"User-Agent" =>
+				"WordPress/" . get_bloginfo("version") . "; " . get_bloginfo("url"),
+		],
+	]);
 
 	if (
-		is_wp_error( $response ) ||
-		200 !== wp_remote_retrieve_response_code( $response )
+		is_wp_error($response) ||
+		200 !== wp_remote_retrieve_response_code($response)
 	) {
-		return false;
+		$cached_remote = false;
+		return $cached_remote;
 	}
 
-	$content = wp_remote_retrieve_body( $response );
-	$remote  = json_decode( $content );
+	$remote = json_decode(wp_remote_retrieve_body($response));
 
-	if ( ! $remote || ! isset( $remote->version ) ) {
-		return false;
+	if (!$remote || !isset($remote->version)) {
+		$cached_remote = false;
+		return $cached_remote;
 	}
 
-	$remote->download_url = JCORE_MAAILMA_RELEASE . '/' . $remote->name . 'zip';
+	if (!empty($remote->name)) {
+		$remote->download_url =
+			JCORE_MAAILMA_RELEASE . "/" . $remote->name . ".zip";
+	} else {
+		$remote->download_url = "";
+	}
 
-	return $remote;
+	$cached_remote = $remote;
+	return $cached_remote;
 }
